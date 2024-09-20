e1a790c4a96bee39deafa0dc9ca6dcfe9bcc6fb0
Hugo Reymond <hugo.reymond@mailo.fr>
2024-09-21 00:00:05 +0200

    Added README, added drag'n'drop prototype and cleaned template
    - Added README.md
    - Tested Godot Drag'n'drop example
    - Removed usuned files

---
+++ README.txt
@@ -0,0 +1,22 @@
+# Cowid 24: Flatten the Curve
+
+You're the only one who can save our livestock by containing the Cowid 24 epidemic.  The aim of the
+game is simple: to keep the contamination rate below the target while a vaccine is being produced.
+
+To do this, you can move contaminated cows into quarantine so that they don't contaminate the rest 
+of the herd. Be careful however, as the situation can easily turn into a complete chaos !
+
+Controls : Simply drag and drop the cows
+
+Cows that fart are sick and will contaminate the rest of the herd. Beware, however: healthy cows
+that are contaminated will remain infectious but symptom-free before they start farting themselves.
+
+-----
+
+TODO:
+	- [ ] Un glisser déposer pour un Sprite2D
+		- [ ] Gérer le cas où deux vaches sont aux même endroit
+	- [ ] Mouvement des personnes (IA)
+	- [ ] État interne des personnes
+	- [ ] Système pour le compte des malades et le seuil
+	- [ ] Système de contamination
--- scripts/init.gd
+++ scripts/init.gd
@@ -1,14 +1,31 @@
 extends Node2D
 
-@onready var dialog: Node2D = $Dialog
+const CAMERA_ZOOM = 2
+var dragging = false
+var click_radius = 32 # Size of the sprite.
+
+@onready var vache: AnimatedSprite2D = $Vache
+
+func _input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if (event.position - $Vache.position*CAMERA_ZOOM).length() < click_radius:
+
+			# Start dragging if the click is on the sprite.
+			if not dragging and event.pressed:
+				dragging = true
+		# Stop dragging if the button is released.
+		if dragging and not event.pressed:
+			dragging = false
+
+	if event is InputEventMouseMotion and dragging:
+		# While dragging, move the sprite with the mouse.
+		$Vache.position = event.position /CAMERA_ZOOM
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	dialog.start_dialog("res://assets/Dialogs/first_dialog.txt")
+	print("Hello world !")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_up"):
-		print("Going up !")
-		dialog.start_dialog("res://assets/Dialogs/first_dialog.txt")
+	pass
