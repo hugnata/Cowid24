236b2c622ebfbb383824c27f909c60cd14b1b872
Hugo Reymond <hugo.reymond@mailo.fr>
2024-09-21 12:26:13 +0200

    Added Cow spawning mecanism and commented drag'n'drop implementation

---
+++ scripts/cow_manager.gd
@@ -0,0 +1,33 @@
+extends Node
+
+# The approximated cow size, in pixels (to ensure that cow do no spawn on fences)
+@export var COW_SIZE = 20
+# The area the cow will spawn in. The shape of the CollisionShape2D must be a rectangle
+@export var spawning_area : CollisionShape2D 
+# The node that will contain all the cow as childrens
+@export var cows_node : Node
+
+@onready var rng = RandomNumberGenerator.new()
+
+func _ready() -> void:
+	assert(spawning_area.shape.get_class()=="RectangleShape2D" && 
+	"The cow manager script only handles Rectangle Spawning Area for now")
+	assert(cows_node != null && "You must provide a node to contains the cows !")
+	
+func spawn_cows(number: int):
+	for i in range(number):
+		spawn_cow()
+
+func spawn_cow():
+	print("Hello, I'm a cow ")
+	var cow_scene = preload("res://scenes/vache.tscn")
+	var spawning_rectangle : Rect2 = spawning_area.shape.get_rect()
+	var position = spawning_area.get_global_transform().get_origin() + spawning_rectangle.position
+	var size = spawning_rectangle.size
+	var x = randi_range(position.x+COW_SIZE,position.x + size.x  - COW_SIZE)
+	var y = randi_range(position.y+COW_SIZE,position.y + size.y  - COW_SIZE)
+	var cow: Node2D = cow_scene.instantiate()
+	cow.set_global_position(Vector2(x,y))
+	cows_node.add_child(cow)
+	
+	
--- scripts/init.gd
+++ scripts/init.gd
@@ -4,26 +4,28 @@ const CAMERA_ZOOM = 2
 var dragging = false
 var click_radius = 32 # Size of the sprite.
 
-@onready var vache: AnimatedSprite2D = $Vache
+@onready var cow_manager: Node = $"Cow Manager"
 
-func _input(event):
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		if (event.position - $Vache.position*CAMERA_ZOOM).length() < click_radius:
-
-			# Start dragging if the click is on the sprite.
-			if not dragging and event.pressed:
-				dragging = true
-		# Stop dragging if the button is released.
-		if dragging and not event.pressed:
-			dragging = false
-
-	if event is InputEventMouseMotion and dragging:
-		# While dragging, move the sprite with the mouse.
-		$Vache.position = event.position /CAMERA_ZOOM
+## Drag'n'drop logic
+#func _input(event):
+	#if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		#if (event.position - $Vache.position*CAMERA_ZOOM).length() < click_radius:
+#
+			## Start dragging if the click is on the sprite.
+			#if not dragging and event.pressed:
+				#dragging = true
+		## Stop dragging if the button is released.
+		#if dragging and not event.pressed:
+			#dragging = false
+#
+	#if event is InputEventMouseMotion and dragging:
+		## While dragging, move the sprite with the mouse.
+		#$Vache.position = event.position /CAMERA_ZOOM
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print("Hello world !")
+	cow_manager.spawn_cows(5)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
